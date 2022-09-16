b0cf3d4669d0f31fddbd93f31a6b966e5e3536af
HushKish <eliace3749@gmail.com>
2022-09-16 10:36:04 +0900

    6c

--- src/main/java/WordCRUD.java
+++ src/main/java/WordCRUD.java
@@ -47,14 +47,29 @@ public class WordCRUD implements ICRUD{
         }
         System.out.println("-------------------------");
     }
+    public ArrayList<Integer> listAll(String key){
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j = 0;
+        System.out.println("-------------------------");
+        for(int i = 0; i < list.size(); i++){
+            String word = list.get(i).getWord();
+            if(!word.contains(key))continue;
+            System.out.print((j+1)+" ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("-------------------------");
+        return idlist;
+    }
 
     public void updateItem() {
         System.out.print("=> 수정할 단어 검색 :");
         String keyword = s.next();
-        ArrayList<int> idlist = this.listAll(keyword);
+        ArrayList<Integer> idlist = this.listAll(keyword);
         System.out.print("=> 수정할 번호 선택 : ");
         int id = s.nextInt();
-        s.nextInt();
+//        s.nextInt();
         System.out.print("=> 뜻 입력 : ");
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
